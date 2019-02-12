0e5ff36604521307a00e77a7582060cd429bd0e5
dnagis <vincent.achard@gmail.com>
2019-02-12 21:58:57 +0100

    nouvelle technique plus propre pour lire bdd

--- sqlite/vvnx.cpp
+++ sqlite/vvnx.cpp
@@ -4,10 +4,12 @@
 * 
 * adb push out/target/product/mido/system/bin/sqlvvnx /system/bin
 * 
-* CREATE TABLE temp (ID INTEGER PRIMARY KEY AUTOINCREMENT, ALRMTIME INTEGER NOT NULL, MAC TEXT NOT NULL, TEMP REAL NOT NULL);
-* insert into temp values(
 * 
-* insert into temp values(NULL,11111111, 'eee', 234);
+* https://stackoverflow.com/questions/31146713/sqlite3-exec-callback-function-clarification
+* 
+* CREATE TABLE temp (ID INTEGER PRIMARY KEY AUTOINCREMENT, ALRMTIME INTEGER NOT NULL, MAC TEXT NOT NULL, TEMP REAL NOT NULL);
+* 
+* 
 * 
 * sqlite3 /data/data/com.example.android.bluealrm/databases/temp.db "select * from temp;"
 * 
@@ -24,73 +26,52 @@
 //sans libsqlite à LOCAL_SHARED_LIBRARIES du Android.mk les includes sqlite3 plantent!
 #include <sqlite3.h>
 
-static int callback(void *data, int argc, char **argv, char **azColName){
-   int i;
-   fprintf(stderr, "%s: ", (const char*)data);
-   
-   for(i = 0; i<argc; i++){
-      printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
-   
-   printf("\n");
-   return 0;
-}
-
-
 
 
 int main()
 {
 	sqlite3 *db;
-	char *zErrMsg = 0;
+   	sqlite3_stmt *stmt;
 	int rc;
-	const char* data = "Callback function called";
+
+	rc = sqlite3_open("/data/data/com.example.android.bluealrm/databases/temp.db", &db);  
+	char const *sql = "select * from temp WHERE ALRMTIME=1549708291;";
 	
-	/**lire une base de donnees **/
+	
+	rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);	
+	if (rc != SQLITE_OK) {
+		printf("error: %s", sqlite3_errmsg(db));
+		return 1;
+	}
+	
+	while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
+		int id           = sqlite3_column_int (stmt, 0);
+		const unsigned char *mac = sqlite3_column_text(stmt, 2);
+		printf("resultat: %i, %s \n", id, mac);
+	}
+	
+	if (rc != SQLITE_DONE) {
+	printf("error: %s", sqlite3_errmsg(db));
+	}
+	
+	sqlite3_finalize(stmt);
 
-
-
-
-   /* Open database */
-   rc = sqlite3_open("/data/data/com.example.android.bluealrm/databases/temp.db", &db);
-   
-   if( rc ) {
-      fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-      return(0);
-   } else {
-      fprintf(stderr, "Opened database successfully\n");
-   }
-
-   /* Create SQL statement -- gaffe à la taille du char[] */
+	
+	/**lire une base de donnees technique avec callback d'un tuto, super lourd
+	char *zErrMsg = 0; 
    char sql[50] = "select * from temp where ID=234;";
-
-   /* Execute SQL statement */
-   rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
-   
-   if( rc != SQLITE_OK ) {
-      fprintf(stderr, "SQL error: %s\n", zErrMsg);
-      sqlite3_free(zErrMsg);
-   } else {
-      fprintf(stdout, "Operation done successfully\n");
-   }
-   sqlite3_close(db);
-
-
-    
-
-		
+   rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);**/		
 
 	/**écriture dans bdd
-	 * 	sqlite3 *db;
-	int rc;
 	char stmt[70] = "";
 	char debut_stmt[] = "insert into temp values(NULL,11111111, 'eee', 123);";
 	strcpy(stmt, debut_stmt);	
 	printf("commande en string = %s\n", stmt);		
 	rc = sqlite3_open("/data/data/com.example.android.bluealrm/databases/temp.db", &db);
 	rc = sqlite3_exec(db, stmt, NULL, 0, NULL); 			
-	sqlite3_close(db);**/
-    
+	**/
+    sqlite3_close(db);
+
 
     _exit(0);
 }
