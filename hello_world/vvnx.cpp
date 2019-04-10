428adf70392cdaae22bb195d6ee2619bd10c59f8
dnagis <vincent.achard@gmail.com>
2019-04-10 18:26:13 +0200

    hello world épuré à l'extrême!

--- hello_world/vvnx.cpp
+++ hello_world/vvnx.cpp
@@ -1,6 +1,5 @@
 /*
 ***
-* frameworks/base/cmds/
 * 
 * adb push out/target/product/mido/system/bin/hellovvnx /system/bin
 * 
@@ -8,33 +7,16 @@
 * 
  */
 
-#include <errno.h>
-#include <unistd.h>
 #include <stdio.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <string.h>
 
-#include <binder/IServiceManager.h>
 
-using namespace android;
 
 
 
 int main()
 {
-	const String16 name("alarm");
-	//const String16 name("ZoubZoub"); //contrôle neg
 
+    printf("Hello World!\n");
 
-    fprintf(stderr, "Début de main de natvvnx, on va se coller au service...\n");
-    
-    sp<IServiceManager> sm = defaultServiceManager();
-    if (sm->checkService(name) == nullptr) {
-		fprintf(stderr, "Le check du service a foiré\n");
-		_exit(1);
-	}
-    
-
-    _exit(0);
+    return 0;
 }
